09f405b02de835a7d57e2915963fe9f743b5f805
Liana Eka Cahya Ningsih <50134585+Lianaeka@users.noreply.github.com>
2019-05-28 10:39:30 +0700

    Add files via upload

---
+++ Admin.php
@@ -0,0 +1,27 @@
+<!DOCYTE html>
+<html>
+<head>
+	<tittle> Login Admin |	SIBOS<tittle>
+</head>
+
+<body>
+	<header>
+		<h3>Admin SIBOS</h3>
+	</header>
+	<form action="proses-pendaftaran.php" method="POST">
+		<fieldset>
+		<p>
+			<label for="Username">Username: </label>
+			<input type="text" name="Username" placeholder="Username" />	
+		</p>
+		<p>
+		<label for="Password">Password: </label>
+			<input type="text" name="Password" placeholder="Password" />
+		</p>
+		<p>		
+			<input type="submit" value="Masuk" name="Masuk" />
+		</p>
+		</fieldset>
+	</form>
+	</body>
+</html>	
